fa541fdb57b43f777e3d84a48f3380d4f1d6049b
dineshbagne <dineshrbagne@gmail.com>
2023-02-09 10:46:53 +0530

    updating ./learn-terrafarm

---
+++ 02-data-sources/data.tf
@@ -0,0 +1,15 @@
+data "aws_ami" "example"{
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = [973714476881]
+}
+output "ami" {
+  value = data.aws_ami.example
+}
+
+data "aws_instance" "foo"{
+  instance_id = "i-0b78cc4d8c16b94d7"
+}
+output "instance" {
+  value = data.aws_instance.foo.public_ip
+}
