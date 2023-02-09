404a995237f72d046cd71c3363adf92b78210df5
dineshbagne <dineshrbagne@gmail.com>
2023-02-09 11:24:32 +0530

    updating ./learn-terrafarm

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -16,3 +16,21 @@ variable "bool" { default = false }
 output "option" {
   value = var.bool
 }
+#defaultvarible type
+variable "sample4" { default = "Bagne"}
+
+#map variable type
+variable "maptype" {
+  default = {
+    number = 100
+    string = "DineshBagne"
+    boolean = false
+  }
+}
+
+output "map" {
+  value = var.maptype.string
+}
+output "map1" {
+  value = var.maptype.number
+}
